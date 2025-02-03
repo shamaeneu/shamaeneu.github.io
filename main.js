be67dc8198cf3ca31081a2d79af4cdd9d47cb3c7
shamaeneu <shannonmaeneumann@gmail.com>
2025-02-03 13:44:52 +0200

    mains

---
+++ main.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie'), // Required
+    path: 'json/SZ_Intro_Anim.json', // Required
+    renderer: 'svg', // Required
+    loop: true, // Optional
+    autoplay: true, // Optional
+    name: "Intro", // Name for future reference. Optional.
+  })
